4c5ac01cb1d0393ca6c8e003d19b07ca0f853eaf
cmd-not-found <cmd404notfound@gmail.com>
2022-03-11 00:03:22 -0500

    combine example into cohesive flow

--- README.md
+++ README.md
@@ -1,8 +1,69 @@
 # etsy-oauthapi-node
 
+This is an example `node` implementation of Etsy's new v3 Oauth API. The code here is a modified version of Etsy's quickstart tutorial with all components packaged into a simple `server.js` file. The end result of this app will be a completed Oauth flow with valid refresh and access tokens.
+
+> **REF:** <https://developers.etsy.com/documentation/tutorials/quickstart>
+
+## Requirements
+1. An Etsy approved app. Request one [here](https://www.etsy.com/developers/register).
+2. After approved (could take several days), update your redirect URI on the Etsy manage my apps page [here](https://www.etsy.com/developers/your-apps). If you don't know what to put, input `http://localhost:3003`. That's what we use in this example.
+
+> **NOTE:** `v3` of Etsy's API is new and all new apps (as of early 2022) must use this version. See timeline [here](https://developers.etsy.com/documentation/migration/index/#launch-stages).
+
+## Setup
+
 ```sh
-% npm install
-% node server.js 
-Example app listening at http://localhost:3003
-^C
+$ git clone git@github.com:cmd-not-found/etsy-oauthapi-node.git
+$ cd ./etsy-oauthapi-node/etsyapp
+$ npm install
 ```
+
+## Configure `.env`
+
+> **REF:** <https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786>
+
+In your `etsyapp/` directory, add a `.env` file with contents similar to below except adjust for your API keystring.
+
+```
+# API
+API_TOKEN=1aa2bb33c44d55eeeeee6fff
+```
+
+## Auth Flow
+
+Start the server.
+
+```sh
+$ node server.js
+```
+
+Navigate to your web app's URL. If you haven't made any modifications to this project, your URL will be:
+
+> <http://localhost:3003>
+
+Click the **Authenticate with Etsy** link on the web page. This will redirect you to Etsy to authenticate. Once authenticated, the flow will take you back to the the redirect URI you specified earlier 
+
+> **REF:** <https://developers.etsy.com/documentation/essentials/authentication#redirect-uris>
+
+If everything went well, you'll see in your web browser JSON that looks like this:
+
+```json
+{
+    "access_token":"12345678.REALLLYlong-stringwithnumbersANDUPPERcaseletters_andsomeunder_scores_0324B1a",
+    "token_type":"Bearer",
+    "expires_in":3600,
+    "refresh_token":"12345678.REALLLYlong-stringwithnumbersANDUPPERcaseletters_andsomeunder_scores_0324B1a"
+}
+```
+
+You can now use this access token to make subsequent API calls to the various endpoints that are applicable for the scope that was requested in the authentication flow. By default, this example app requests the following scopes: `email_r`, `shops_r`, `profile_r`, and `transactions_r`. The scopes are defined in `server.js` with the following statement:
+
+```js
+const contexts = 'email_r%20shops_r%20profile_r%20transactions_r'
+```
+
+For more informatin on scopes, read [here](https://developers.etsy.com/documentation/essentials/authentication#scopes). 
+
+## References
+
+> **REF:** <https://medium.com/programming-sage/handlebars-in-node-js-tutorial-a30a41fc6206>
--- etsyapp/server.js
+++ etsyapp/server.js
@@ -1,16 +1,44 @@
 // Import the express and fetch libraries
 const express = require('express');
 const fetch = require("node-fetch");
-
-// REF 
-// https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
+const crypto = require("crypto");
 const dotenv = require('dotenv');
+const hbs = require("hbs");
+
+// REF Constants
+const port = 3003;
+const baseurl = `http://localhost:${port}`
 dotenv.config();
 const apitoken = process.env.API_TOKEN;
-// REF
+const contexts = 'email_r%20shops_r%20profile_r%20transactions_r'
 
 // Create a new express application
 const app = express();
+app.set("view engine", "hbs");
+app.set("views", `${process.cwd()}/views`);
+
+// The next two functions help us generate the code challenge
+// required by Etsy’s OAuth implementation.
+const base64URLEncode = (str) =>
+  str
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+
+const sha256 = (buffer) => crypto.createHash("sha256").update(buffer).digest();
+
+// We’ll use the verifier to generate the challenge.
+// The verifier needs to be saved for a future step in the OAuth flow.
+const codeVerifier = base64URLEncode(crypto.randomBytes(32));
+
+// With these functions, we can generate
+// the values needed for our OAuth authorization grant.
+const codeChallenge = base64URLEncode(sha256(codeVerifier));
+const state = Math.random().toString(36).substring(7);
+
+const buildMyUrl = `https://www.etsy.com/oauth/connect?response_type=code&redirect_uri=${baseurl}/oauth/redirect&scope=${contexts}&client_id=${apitoken}&state=${state}&code_challenge=${codeChallenge}&code_challenge_method=S256`
+console.log(`Build URL: ${buildMyUrl}`);
 
 // Send a JSON response to a default get request
 app.get('/ping', async (req, res) => {
@@ -34,8 +62,52 @@ app.get('/ping', async (req, res) => {
     }
 });
 
+// This renders our `index.hbs` file.
+app.get('/', async (req, res) => {
+    res.render("index", { customUrl : buildMyUrl });
+});
+
+/**
+These variables contain your API Key, the state sent
+in the initial authorization request, and the client verifier compliment
+to the code_challenge sent with the initial authorization request
+*/
+const clientID = apitoken;
+const clientVerifier = codeVerifier;
+const redirectUri = `${baseurl}/oauth/redirect`;
+
+app.get("/oauth/redirect", async (req, res) => {
+    // The req.query object has the query params that Etsy authentication sends
+    // to this route. The authorization code is in the `code` param
+    const authCode = req.query.code;
+    const tokenUrl = 'https://api.etsy.com/v3/public/oauth/token';
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+            grant_type: 'authorization_code',
+            client_id: clientID,
+            redirect_uri: redirectUri,
+            code: authCode,
+            code_verifier: clientVerifier,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = await fetch(tokenUrl, requestOptions);
+
+    // Extract the access token from the response access_token data field
+    if (response.ok) {
+        const tokenData = await response.json();
+        res.send(tokenData);
+    } else {
+        res.send("oops");
+    }
+});
+
+
 // Start the server on port 3003
-const port = 3003;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at ${baseurl}`);
 });
